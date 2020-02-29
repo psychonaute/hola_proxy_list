514a76efd751bc7a7d8a3effccb2d78c66d097a5
Vladislav Yarmak <vladislav@vm-0.com>
2020-02-29 14:53:36 +0200

    WIP

---
+++ hola-proxy.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import argparse
+import enum
+import logging
+import uuid
+import urllib.request
+import urllib.parse
+import codecs
+import json
+import random
+import pprint
+
+USER_AGENT = "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
+EXT_VER = "1.164.641"
+EXT_BROWSER = "chrome"
+PRODUCT = "cws"
+CCGI_URL = "https://client.hola.org/client_cgi/"
+
+def setup_logger(name, verbosity):
+    logger = logging.getLogger(name)
+    logger.setLevel(verbosity)
+    handler = logging.StreamHandler()
+    handler.setLevel(verbosity)
+    handler.setFormatter(logging.Formatter("%(asctime)s "
+                                           "%(levelname)-8s "
+                                           "%(name)s: %(message)s",
+                                           "%Y-%m-%d %H:%M:%S"))
+    logger.addHandler(handler)
+    return logger
+
+class LogLevel(enum.IntEnum):
+    debug = logging.DEBUG
+    info = logging.INFO
+    warn = logging.WARN
+    error = logging.ERROR
+    fatal = logging.FATAL
+    crit = logging.CRITICAL
+
+    def __str__(self):
+        return self.name
+
+def fetch_url(url, *, data=None, method=None, timeout=10):
+    logger = logging.getLogger("FETCH")
+    logger.info("Fetching URL %s with method %s, post data=%s",
+                url, method, repr(data))
+    http_req = urllib.request.Request(
+        url,
+        data=data,
+        headers={
+            "User-Agent": USER_AGENT
+        }
+    )
+    with urllib.request.urlopen(http_req, None, timeout) as resp:
+        coding = resp.headers.get_content_charset()
+        coding = coding if coding is not None else "utf-8-sig"
+        decoder = codecs.getreader(coding)(resp)
+        res = decoder.read()
+    return res
+
+def encode_params(params, encoding=None):
+    return urllib.parse.urlencode(params, encoding=encoding)
+
+def background_init(user_uuid):
+    post_data = encode_params({
+        "login": "1",
+        "ver": EXT_VER,
+    }).encode('ascii')
+    query_string = encode_params({
+        "uuid": user_uuid,
+    })
+    resp = fetch_url(CCGI_URL + "background_init?" + query_string,
+                     data=post_data)
+    return json.loads(resp)
+
+def zgettunnels(user_uuid, session_key, country="us", *, limit=3, is_premium=0):
+    qs = encode_params({
+        "country": country,
+        "limit": limit,
+        "ping_id": random.random(),
+        "ext_ver": EXT_VER,
+        "browser": EXT_BROWSER,
+        "product": PRODUCT,
+        "uuid": user_uuid,
+        "session_key": session_key,
+        "is_premium": is_premium,
+    })
+    resp = fetch_url(CCGI_URL + "zgettunnels?" + qs)
+    return json.loads(resp)
+
+def parse_args():
+    def check_loglevel(arg):
+        try:
+            return LogLevel[arg]
+        except (IndexError, KeyError):
+            raise argparse.ArgumentTypeError("%s is not valid loglevel" % (repr(arg),))
+
+    def check_positive_int(arg):
+        def fail():
+            raise argparse.ArgumentTypeError("%s is not valid positive integer" % (repr(arg),))
+        try:
+            ivalue = int(arg)
+        except ValueError:
+            fail()
+        if ivalue <= 0:
+            fail()
+        return ivalue
+
+    parser = argparse.ArgumentParser(
+        description="Fetches free proxy list via Hola browser extension API",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-l", "--list-countries",
+                        action="store_true",
+                        help="list available countries")
+    parser.add_argument("-c", "--country",
+                        default="us",
+                        help="desired proxy location")
+    parser.add_argument("-n", "--limit",
+                        default=3,
+                        type=check_positive_int,
+                        help="amount of proxies in retrieved list")
+    parser.add_argument("-v", "--verbosity",
+                        help="logging verbosity",
+                        type=check_loglevel,
+                        choices=LogLevel,
+                        default=LogLevel.info)
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    logger = setup_logger("MAIN", args.verbosity)
+    user_uuid = uuid.uuid4().hex
+    session_key = background_init(user_uuid)["key"]
+    pprint.pprint(zgettunnels(user_uuid, session_key,
+                              country=args.country, limit=args.limit))
+
+
+if __name__ == "__main__":
+    main()
